0b4e45bf3e4caf9ee5af401fd9d0ec09d4c565b2
yponion <yp071704@gmail.com>
2024-12-16 00:45:35 +0900

    feat: end of section2-클라이언트 컴포넌트로 전환하기

---
+++ src/app/(afterLogin)/home/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <main>홈 페이지</main>;
+}
---
+++ src/app/(afterLogin)/layout.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+export default function AfterLoginLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  return <div>{children}</div>;
+}
