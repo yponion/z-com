cb7320a79587a8accc1cf70be7f91593087c80ec
양정운 <yp@yp-2.local>
2024-12-20 00:21:18 +0900

    feat: Main, SignupModal css 설명
     - router.push()하면 이전 페이지로 갔을 때 무환 루프에 걸릴 수 있다.
     - router.replace() 는 대치이기 때문에 그럴일 없다.

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,5 +1,28 @@
-import { redirect } from "next/navigation";
+// "use client";
+
+// import { useRouter } from "next/navigation";
+// import Main from "../_component/Main";
+
+// export default function Login() {
+//   const router = useRouter();
+//   router.replace("/i/flow/login");
+//   return <Main />;
+// }
+
+// 오류 나서 일단 gpt가 알려준대로 함. -> 강의에서 빌드에서 설명해준다고 함.
+
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import Main from "../_component/Main";
 
 export default function Login() {
-  redirect("/i/flow/login");
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace("/i/flow/login");
+  }, [router]);
+
+  return <Main />;
 }
