b9ee13218fe51eb4f58ff0a60223459ba65911c5
yponion <yp071704@gmail.com>
2024-12-26 22:36:11 +0900

    feat: usePathname과 /explore 페이지
     - pathname을 이용해서 해당 페이지일 때 null을 반환하거나 다른 컴포넌트를 반환

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,3 +1,19 @@
+import SearchForm from "../_component/SearchForm";
+import Trend from "../_component/Trend";
+import style from "./explore.module.css";
+
 export default function Home() {
-  return <div>검색 페이지</div>;
+  return (
+    <main className={style.main}>
+      <div className={style.formZone}>
+        <SearchForm />
+      </div>
+      <div className={style.trend}>
+        <h3>나를 위한 트렌드</h3>
+        <Trend />
+        <Trend />
+        <Trend />
+      </div>
+    </main>
+  );
 }
---
+++ src/app/(afterLogin)/search/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <div>검색 페이지</div>;
+}
