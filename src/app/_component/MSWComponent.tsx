ca50b7b65b6f1d06c15d508056b22590727d8f8f
yponion <yp071704@gmail.com>
2024-12-29 17:41:21 +0900

    next용 msw 컴포넌트
     - [이슈 있음](https://github.com/vercel/next.js/issues/69098)

---
+++ src/app/_component/MSWComponent.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Suspense, use } from "react";
+import { handlers } from "@/mocks/handlers";
+
+const mockingEnabledPromise =
+  typeof window !== "undefined"
+    ? import("@/mocks/browser").then(async ({ default: worker }) => {
+        if (process.env.NODE_ENV === "production") {
+          return;
+        }
+        await worker.start({
+          onUnhandledRequest(request, print) {
+            if (request.url.includes("_next")) {
+              return;
+            }
+            print.warning();
+          },
+        });
+        worker.use(...handlers);
+        (module as any).hot?.dispose(() => {
+          worker.stop();
+        }); // https://github.com/vercel/next.js/issues/69098 이슈로 임시적으로 코드 추가
+        console.log(worker.listHandlers());
+      })
+    : Promise.resolve();
+
+export function MSWProvider({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  // If MSW is enabled, we need to wait for the worker to start,
+  // so we wrap the children in a Suspense boundary until it's ready.
+  return (
+    <Suspense fallback={null}>
+      <MSWProviderWrapper>{children}</MSWProviderWrapper>
+    </Suspense>
+  );
+}
+
+function MSWProviderWrapper({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  use(mockingEnabledPromise);
+  return children;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,20 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { MSWProvider } from "./_component/MSWComponent";
+
+if (
+  process.env.NEXT_RUNTIME === "nodejs" &&
+  process.env.NODE_ENV !== "production"
+) {
+  // const { server } = require('@/mocks/http')
+  // server.listen()
+  // eslint require 대신 ES6 import 사용 규칙 때문에 변경
+  (async () => {
+    const { server } = await import("@/mocks/http");
+    server.listen();
+  })();
+}
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +39,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <MSWProvider>{children}</MSWProvider>
       </body>
     </html>
   );
