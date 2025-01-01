e39bec2c9e394e84704c4efcd5ff2e0b9d3569cd
yponion <yp071704@gmail.com>
2025-01-01 20:46:53 +0900

    feat: react-query SSR 설정하기
     - npm i @tanstack/react-query@5
     - npm i @tanstack/react-query-devtools@5 -D
     - ReactQuery Provider 설정

---
+++ src/app/(afterLogin)/_component/RQProvider.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useState } from "react";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+type props = {
+  children: React.ReactNode;
+};
+
+function RQProvider({ children }: props) {
+  const [client] = useState(
+    new QueryClient({
+      defaultOptions: {
+        // react-query 전역 설정
+        queries: {
+          refetchOnWindowFocus: false,
+          retry: false,
+        },
+      },
+    })
+  );
+
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools
+        initialIsOpen={process.env.NEXT_PUBLIC_MOD === "local"}
+      />
+    </QueryClientProvider>
+  );
+}
+
+export default RQProvider;
--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,19 +1,50 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 import Post from "../_component/Post";
 import PostForm from "./_component/PostForm";
 import Tab from "./_component/Tab";
 import TabProvider from "./_component/TabProvider";
 import style from "./home.module.css";
 
-export default function Home() {
+async function getPostRecommends() {
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/postRecommends}`,
+    {
+      next: {
+        tags: ["posts", "recommends"],
+      },
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", "recommends"],
+    queryFn: getPostRecommends,
+  });
+  const dehydratedState = dehydrate(queryClient);
+
   return (
     <main className={style.main}>
-      <TabProvider>
-        <Tab />
-        <PostForm />
-        <Post />
-        <Post />
-        <Post />
-      </TabProvider>
+      <HydrationBoundary state={dehydratedState}>
+        <TabProvider>
+          <Tab />
+          <PostForm />
+          <Post />
+          <Post />
+          <Post />
+        </TabProvider>
+      </HydrationBoundary>
     </main>
   );
 }
