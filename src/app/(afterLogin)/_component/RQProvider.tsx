104049e97550591bc3fa6e3b15a1c3721cc6c1ec
yponion <yp071704@gmail.com>
2025-01-02 17:24:11 +0900

    react-query를 쓰는 이유와 fresh, stale, inactive
     - 캐싱을 통해 트래픽을 아낄 수 있고, 사용자가 매우 빠르게 컨텐츠를 볼 수 있다.
     - 로딩, 성공, 실패 interface를 표준화 했다.
     - key system 좋다. 구조를 잘 갖춰 놓으면 편리함.
    
     - Fresh: 이 데이터는 항상 최신 데이터고, 업데이트할 필요가 없다. 서버에서 불러올 필요가 없다.
        데이터가 fresh 상태일 경우 새로고침 시 등의 동작이 있을 경우, 다시 서버에 데이터를 요청하지 않는다.
        언제까지 Fresh 상태를 유지할지는 개발자가 설정할 수 있다.
        react-query의 기본 데이터 상태는 Fresh가 아니다.
        staleTime: Infinity 시 항상 Fresh
     - Stale: 기회가 되면 항상 데이터를 새로 가져와라.
        개발자가 설정한 조건에 따라 데이터를 새로 가져온다.
        전역으로 설정하거나, useQuery 훅에서 별도로 설정할 수도 있다.
        새로고침이 될 경우 서버에 다시 요청해 데이터를 받아온다.
     - Inactive: 사용자가 보고 있는 화면에서 해당 queryKey를 사용하는 query가 쓰이지 않는다면 inactive 상태가 된다.
        예를 들어, [’posts, ‘recommends’] queryKey를 사용하는 query가 쓰이지 않는 ‘탐색하기’ 탭으로 이동한다면, 해당 query는 inactive 상태가 된다.
        inactive 상태가 되면 gcTime이 흘러가기 시작한다.
        따라서 기본적으로 gcTime은 5분으로 설정되어 있기 때문에 query가 inactive 상태가 되면 메모리에서 해당 쿼리 데이터가 정리되어 캐시에서 삭제된다.
     - Fetching: 데이터를 가지고 오는 중
     - Paused: 데이터를 가지고 오는게 일시정지 일 때, 오프라인 상태일 때 볼 수 있다.

--- src/app/(afterLogin)/_component/RQProvider.tsx
+++ src/app/(afterLogin)/_component/RQProvider.tsx
@@ -14,8 +14,10 @@ function RQProvider({ children }: props) {
       defaultOptions: {
         // react-query 전역 설정
         queries: {
-          refetchOnWindowFocus: false,
-          retry: false,
+          refetchOnWindowFocus: false, // 다른 탭에 갔다가 돌아오면 refetch
+          refetchOnMount: true, // 컴포넌트가 마운트되었을 때(또는 다시 마운트되었을 때) refetch
+          refetchOnReconnect: false, // 네트워크 연결이 복구되면 refetch
+          retry: false, // 실패시 재시도
         },
       },
     })
--- src/app/(afterLogin)/home/_component/PostRecommends.tsx
+++ src/app/(afterLogin)/home/_component/PostRecommends.tsx
@@ -9,6 +9,8 @@ export default function PostRecommends() {
   const { data } = useQuery<IPost[]>({
     queryKey: ["posts", "recommends"],
     queryFn: getPostRecommends,
+    staleTime: 60 * 1000, // fresh -> stale, default: 0ms
+    gcTime: 300000, // inactive 일 때 메모리 정리, default: 300000ms
   });
 
   return data?.map((post) => <Post key={post.postId} post={post} />);
