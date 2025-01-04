8a30518a583f3737802caa862082db6b2ee77801
yponion <yp071704@gmail.com>
2025-01-05 02:21:05 +0900

    feat: url rewrites
     - next.config.ts
        rewrite 함수를 이용해 url 주소를 치환 가능하다
     - front에서 img src 앞에다가 localhost:9090 붙여주는 방법도 있음

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
       bodySizeLimit: "10mb",
     },
   },
+  async rewrite() {
+    return [
+      {
+        cource: 'upload/:slug', // 이 주소를 볼 때마다
+        destination: `${process.env.NEXT_PUBLIC_BASE_URL}/upload/:slug}`, // 이 주소로 바꿔서 실행
+      },
+    ]
+  },
 };
 
 export default nextConfig;
