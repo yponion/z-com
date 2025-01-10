20ec1f62e0e104648ea436ad8d0817be8d86d175
yponion <yp071704@gmail.com>
2025-01-11 02:11:09 +0900

    fix: 유저의 포스트 목록 가져오기
     - ?cursor=0 붙이니까 됨
        msw 쓸땐 잘 됐는데 server단에서 뭐 설정 해둬서 400 뜬듯

--- src/app/(afterLogin)/[username]/_lib/getUserPosts.ts
+++ src/app/(afterLogin)/[username]/_lib/getUserPosts.ts
@@ -8,7 +8,7 @@ export const getUserPosts: QueryFunction<
     = async ({ queryKey }) => {
         const [, , username] = queryKey;
         const res = await fetch(
-            `${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${username}/posts`,
+            `${process.env.NEXT_PUBLIC_BASE_URL}/api/users/${username}/posts?cursor=0`,
             {
                 next: {
                     tags: ["posts", "users", username],
